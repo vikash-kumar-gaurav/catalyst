28e5c39dae1f2253a380b8cc15da45e06ed23d5d
vikash gaurav <vikashkumargauravdss@gmail.com>
2025-07-15 16:52:52 +0530

    done with social iniciative

--- components/layout/MainNavbar.tsx
+++ components/layout/MainNavbar.tsx
@@ -128,7 +128,7 @@ export default function MainNavbar({ isFixed = false }: MainNavbarProps) {
                 onMouseLeave={handleMouseLeave}
               >
                 <Link
-                  href={item.href}
+                  href={item?.submenu ? "#" : item.href}
                   className="px-2 xl:px-3 py-2 text-sm font-medium text-gray-800 hover:text-red-600 flex items-center transition-colors duration-300 rounded-md hover:bg-gray-50"
                 >
                   {item.name}
